0ba1260cb80f14f0f0ed74f57bc0c945370d5e70
𝆺꯭𝅥⏤⵿⵿꯭꤫꤫︭ꭙ‌³⛦⃕͜💫🇲σ᭡፝֟ɳ🌙─‌⃛•𝆺꯭𝅥༎𓆪 <160732506+Moonshining1@users.noreply.github.com>
2024-05-15 14:38:22 +0530

    Update start.py

--- Zaid/modules/bot/start.py
+++ Zaid/modules/bot/start.py
@@ -9,17 +9,17 @@ from pyrogram import *
 from pyrogram.types import * 
 
 PHONE_NUMBER_TEXT = (
-    "✘ Heya My Master👋!\n\n✘ I'm Your Assistant?\n\n‣ I can help you to host Your Left Clients.\n\n‣ Repo: github.com/Itz-Zaid/Zaid-Userbot \n\n‣ This specially for Buzzy People's(lazy)\n\n‣ Now /clone {send your PyroGram String Session}"
+    "✘ Heya My Master👋!\n\n✘ I'm Your Assistant?\n\n‣ I can help you to host Your Left Clients.\n\n‣ [🇲σ᭡፝֟ɳ🌙](https://t.me/Moonshining2) \n\n‣ This specially for Buzzy People's(lazy)\n\n‣ Now /clone {send your PyroGram String Session}"
 )
 
 @app.on_message(filters.user(OWNER_ID) & filters.command("start"))
 async def hello(client: app, message):
     buttons = [
            [
-                InlineKeyboardButton("✘ ᴜᴘᴅᴀᴛᴇꜱ ᴄʜᴀɴɴᴇʟ", url="t.me/TheUpdatesChannel"),
+                InlineKeyboardButton("✘ ᴜᴘᴅᴀᴛᴇꜱ ᴄʜᴀɴɴᴇʟ", url="t.me/Kittyxupdates"),
             ],
             [
-                InlineKeyboardButton("✘ ꜱᴜᴘᴘᴏʀᴛ ɢʀᴏᴜᴘ", url="t.me/TheSupportChat"),
+                InlineKeyboardButton("✘ ꜱᴜᴘᴘᴏʀᴛ ɢʀᴏᴜᴘ", url="t.me/grandxmasti"),
             ],
             ]
     reply_markup = InlineKeyboardMarkup(buttons)
